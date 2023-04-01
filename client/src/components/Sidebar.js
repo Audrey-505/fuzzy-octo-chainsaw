12d82adae5d075e123b2155f12a6103219d0a6d0
Audrey Alcantara <adrianaquin22@gmail.com>
2023-04-01 15:31:01 -0400

    Rules Modal added

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { Tab, Nav, Button, Modal } from 'react-bootstrap'
 import './Sidebar.css'
 
 export default function Sidebar() {
-
+const [showModal, setShowModal] = useState(false)
 
     return (
         <div style={{ width: '250px' }} className="d-flex flex-column">
@@ -14,10 +14,38 @@ export default function Sidebar() {
                 <Nav.Link>Rooms</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link>Rules</Nav.Link>
+                <Nav.Link onClick={() => setShowModal(true)}>Rules</Nav.Link>
               </Nav.Item>
             </Nav>
           </Tab.Container>
+
+          <Modal
+          size='lg'
+          show={showModal}
+          onHide={() => setShowModal(false)}>
+            <Tab.Container>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  Rules of App
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Tab.Content>
+                  <h4>Age 13+</h4>
+                  <p>All users must be 13 years of age or older</p>
+                  <h4>Ethics</h4>
+                  <p>in an effort to conduct our platform in an ethical way any information or representation deemed harmful, misleading, or behaviour that could place parents or admins in a legal predicament will be removed.</p>
+                  <h4>Privacy</h4>
+                  <p>Being part of this platform requires mutual trust. Authentic, expressive discussions make groups great, but may also be sensitive and private. What's shared in the group should stay in the group.</p>
+                  <h4>No hate speech</h4>
+                  <p>Make sure that everyone feels safe. Bullying of any kind isn't allowed, and degrading comments about things such as race, religion, culture, sexual orientation, gender or identity will not be tolerated.</p>
+                  <h4>Warning</h4>
+                  <p>Choosing to break any of these rules will result in either a warning or a ban from the server at the discretion of the server owner or moderation crew.</p>
+                </Tab.Content>
+              </Modal.Body>
+            </Tab.Container>
+
+          </Modal>
           
           </div>
  )
