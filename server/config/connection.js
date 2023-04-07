7194cfb849ee733aaab86a955d6c949575b465f3
Audrey Alcantara <adrianaquin22@gmail.com>
2023-04-06 20:38:00 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,24 +1,24 @@
-const { connect, connection } = require('mongoose')
+// const { connect, connection } = require('mongoose')
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/chatDB'
-//const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chatDB'
-
-
-connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection
+// // const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/chatDB'
+// const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chatDB'
 
 
-//const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chatDB', {
+// connect(connectionString, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
 // });
 
-//module.exports = mongoose.connection;
+// module.exports = connection
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chatDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
